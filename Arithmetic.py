31225b4a8b0ffe9dc51a1263e38e2cb2b8fd26e0
vijayalaxmi <vijayalaxmiichangi@gmail.com>
2025-10-24 09:35:12 +0530

    Addition & Subtraction

---
+++ Arithmetic.py
@@ -0,0 +1,7 @@
+num1=float(input("Enter the first number:"))
+num2=float(input("Enter the second number:"))
+addition=num1+num2
+subtraction=num1-num2
+print("\n Results")
+print(f"Addition of {num1}+{num2} is {addition}")
+print(f"subtraction of {num1}+{num2} is {subtraction}")
