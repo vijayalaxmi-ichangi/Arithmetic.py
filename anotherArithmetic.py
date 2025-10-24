f23764acbf45dd1c1b72324cffb5ea4c5a62ccb1
vijayalaxmi <vijayalaxmiichangi@gmail.com>
2025-10-24 10:16:46 +0530

    added new feature

---
+++ anotherArithmetic.py
@@ -0,0 +1,11 @@
+num1=float(input("Enter the first number:-"))
+num2=float(input("Enter the second number:-"))
+addition=num1+num2
+subtraction=num1-num2
+multiply=num1*num2
+divide=num1/num2
+print("\n Results")
+print(f"Addition of {num1}+{num2} is {addition}")
+print(f"subtraction of {num1}-{num2} is {subtraction}")
+print(f"multiplication of {num1}*{num2} is {multiply}")
+print(f"division of {num1}/{num2} is {divide}")
