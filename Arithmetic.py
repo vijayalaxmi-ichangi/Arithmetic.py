b58d6a3b44b4f6380df22c482e200465839a1a18
vijayalaxmi <vijayalaxmiichangi@gmail.com>
2025-10-24 09:46:08 +0530

    modify the code

--- Arithmetic.py
+++ Arithmetic.py
@@ -1,5 +1,5 @@
-num1=float(input("Enter the first number:"))
-num2=float(input("Enter the second number:"))
+num1=float(input("Enter the first number:-"))
+num2=float(input("Enter the second number:-"))
 addition=num1+num2
 subtraction=num1-num2
 print("\n Results")
